cc7a2126d2ae6f1b46722f94cb6ea84270c1cdb9
RoyGevers <roy@iwize.nl>
2020-09-25 16:53:40 +0200

    Applied Singleton design pattern

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,14 @@ import { Mode } from "./toggleLightDark_OOP"
 
 
 window.addEventListener('load', (e) => {
-  let mode = new Mode();
+  let mode = Mode.getInstance();
   mode.toggleOnLoad();
 
   document.getElementById('mode-switchers')!.addEventListener('click', (e) => {
-    mode = new Mode();
+    mode = Mode.getInstance();
     mode.toggleOnClick();
   });
+
+
 
 });
--- src/toggleLightDark_OOP.ts
+++ src/toggleLightDark_OOP.ts
@@ -1,9 +1,30 @@
-//type Theme = 'dark' | 'light';
+
+/* 
+Question:
+
+Door het Singleton patroon werkt het initialiseren van startTheme vanuit localStorage (of systeeminstellingen) in de constructor niet meer. 
+Na één keer klikken blijft de waarde van startTheme en dus oppositeTheme gelijk. 
+
+Hiervoor zijn (tenminste) twee oplossingen. Optie 1:
+In de toggle() method geen parameters gebruiken, maar steeds storedTheme steeds opnieuw uit localStorage halen en vanuit daar startTheme en oppositeTheme vullen. 
+niet erg DRY, dus in eerste instantie ben ik voor optie 2 gegaan:
+In de toggleOnClick() method bij elke iteratie startTheme en OppositeTheme omdraaien. (Hiervoor returnt toggle() nu de thema's steeds)
+
+Welke optie is beter? Of is er nog een betere optie?
+
+*/
+
+
+// Types
 const enum Theme {
     DARK = 'dark',
     LIGHT = 'light'
 }
 
+type Toggle = (before: Theme, after: Theme) => Theme[];
+
+
+// Class
 export class Mode {
     startTheme: Theme;
     oppositeTheme: Theme;
@@ -12,16 +33,26 @@ export class Mode {
     systemPreference: Theme;
     availableThemes: readonly Theme[];
 
-    constructor() {
+    private static instance: Mode;
+
+    private constructor() {
         this.availableThemes = [Theme.LIGHT, Theme.DARK];
-        this.storedTheme = localStorage.getItem('theme') as Theme;
         this.systemPreference = window.matchMedia("(prefers-color-scheme: dark)").matches ? Theme.DARK : Theme.LIGHT;
+        this.button = document.getElementById('mode-switchers')!;
+        this.storedTheme = localStorage.getItem('theme') as Theme;
         this.startTheme = this.storedTheme || this.systemPreference;
         this.oppositeTheme = this.availableThemes.find(t => t !== this.startTheme) as Theme;
-        this.button = document.getElementById('mode-switchers')!;
     }
 
-    private toggle(before: Theme, after: Theme) {
+    public static getInstance(): Mode {
+        if (!Mode.instance) {
+            Mode.instance = new Mode();
+        }
+
+        return Mode.instance;
+    }
+
+    private toggle: Toggle = (before, after) => {
         document.querySelectorAll(`[class*='${before}']`).forEach((elem) => {
             const getMode = [...elem.classList].find((c) => c.includes(before))!;
             elem.classList.replace(getMode, getMode.replace(before, after as Theme));
@@ -29,14 +60,17 @@ export class Mode {
 
         localStorage.setItem('theme', after as Theme);
         this.button.style.transform = (before === Theme.LIGHT) ? 'rotate(90deg)' : 'rotate(0deg)';
+
+        return [after, before]; // Switch the themes every go around
     }
 
     toggleOnLoad() {
-        this.toggle(this.oppositeTheme, this.startTheme); // On load, classes with the old theme are changed based on localStorage || system preference
+        this.toggle(this.oppositeTheme, this.startTheme); // On load, any classes with the old theme are changed based on localStorage || system preference
+
     }
 
     toggleOnClick() {
-        this.toggle(this.startTheme, this.oppositeTheme); // On click, any current theme is replaced with its opposite
+        [this.startTheme, this.oppositeTheme] = this.toggle(this.startTheme, this.oppositeTheme); // On click, any current theme is replaced with its opposite
     }
 }
 
